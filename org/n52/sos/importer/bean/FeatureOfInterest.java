93c52a64f6724a599f77146ea69b93d7344e063d
RSC-52n <r.schnuerer@52north.org>
2011-05-26 12:06:36 +0000

    beans aufgeräumt

---
+++ org/n52/sos/importer/bean/FeatureOfInterest.java
@@ -0,0 +1,5 @@
+package org.n52.sos.importer.bean;
+
+public class FeatureOfInterest extends TableConnection {
+
+}
---
+++ org/n52/sos/importer/bean/ObservedProperty.java
@@ -0,0 +1,5 @@
+package org.n52.sos.importer.bean;
+
+public class ObservedProperty extends TableConnection {
+
+}
