d43674acc4142f05ad00fea97b172d0938fe296d
EHJ-52n <e.h.juerrens@52north.org>
2012-03-06 13:55:50 +0000

    - started configuration of assemble plugin - added license management plugin

--- src/main/java/org/n52/sos/importer/controller/BackNextController.java
+++ src/main/java/org/n52/sos/importer/controller/BackNextController.java
@@ -1,3 +1,26 @@
+/**
+ * Copyright (C) 2012
+ * by 52North Initiative for Geospatial Open Source Software GmbH
+ *
+ * Contact: Andreas Wytzisk
+ * 52 North Initiative for Geospatial Open Source Software GmbH
+ * Martin-Luther-King-Weg 24
+ * 48155 Muenster, Germany
+ * info@52north.org
+ *
+ * This program is free software; you can redistribute and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; even without the implied
+ * WARRANTY OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program (see gnu-gpl v2.txt). If not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
+ * visit the Free Software Foundation web page, http://www.fsf.org.
+ */
 package org.n52.sos.importer.controller;
 
 import org.n52.sos.importer.interfaces.StepController;
@@ -13,11 +36,13 @@ public class BackNextController {
 	
 	private static BackNextController instance = null;
 
-	private BackNextModel backNextModel = new BackNextModel();
+	private BackNextModel backNextModel = null;
 	
-	private BackNextPanel backNextPanel = BackNextPanel.getInstance();
+	private BackNextPanel backNextPanel = null;
 	
 	private BackNextController() {
+		this.backNextModel = new BackNextModel();
+		this.backNextPanel = BackNextPanel.getInstance();
 	}
 
 	public static BackNextController getInstance() {
@@ -26,8 +51,8 @@ public class BackNextController {
 		return instance;
 	}
 	
-	public void setBackButtonVisible(boolean aFlag) {
-		backNextPanel.setBackButtonVisible(aFlag);
+	public void setBackButtonVisible(boolean isBackButtonVisible) {
+		backNextPanel.setBackButtonVisible(isBackButtonVisible);
 	}
 	
 	public void changeNextToFinish() {
@@ -38,8 +63,8 @@ public class BackNextController {
 		backNextPanel.changeFinishToNext();
 	}
 	
-	public void setFinishButtonEnabled(boolean aFlag) {
-		backNextPanel.setFinishButtonEnabled(aFlag);
+	public void setFinishButtonEnabled(boolean isFinishButtonEnabled) {
+		backNextPanel.setFinishButtonEnabled(isFinishButtonEnabled);
 	}
 	
 	public void backButtonPressed() {
@@ -57,21 +82,25 @@ public class BackNextController {
 		currentSC.saveSettings();
 		backNextModel.addPreviousStepController(currentSC);	//put controller on stack
 		
-		//when has already been to the next step
+		// when has already been to the next step
 		StepController followingSC = backNextModel.getFollowingStepController();
 		if (followingSC != null && followingSC.isStillValid()) {
 			MainController.getInstance().setStepController(followingSC);
 			return;
 		}
 		
-		//next step controller of this type	
+		// used to get 4a,b,c,d steps before getting 5a, for example
+		//
+		// next step controller of this type
 		StepController nextSC = currentSC.getNext(); 
 		if (nextSC != null) {
 			MainController.getInstance().setStepController(nextSC);
 			return;
 		}
-
-		//next step controller of another type
+		// When is this code called? In which situation?
+		// When step n+1 is called after step n
+		//
+		// next step controller of another type
 		nextSC = currentSC.getNextStepController();		
 		
 		while (!nextSC.isNecessary())
--- src/main/java/org/n52/sos/importer/controller/Step1Controller.java
+++ src/main/java/org/n52/sos/importer/controller/Step1Controller.java
@@ -1,3 +1,26 @@
+/**
+ * Copyright (C) 2012
+ * by 52North Initiative for Geospatial Open Source Software GmbH
+ *
+ * Contact: Andreas Wytzisk
+ * 52 North Initiative for Geospatial Open Source Software GmbH
+ * Martin-Luther-King-Weg 24
+ * 48155 Muenster, Germany
+ * info@52north.org
+ *
+ * This program is free software; you can redistribute and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; even without the implied
+ * WARRANTY OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program (see gnu-gpl v2.txt). If not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
+ * visit the Free Software Foundation web page, http://www.fsf.org.
+ */
 package org.n52.sos.importer.controller;
 
 import java.io.BufferedReader;
@@ -47,6 +70,8 @@ public class Step1Controller extends StepController {
 		//disable "back" button
 		BackNextController.getInstance().setBackButtonVisible(false);
 		
+		// TODO Set "Next" button inactive
+		
 		String csvFilePath = step1Model.getCSVFilePath();
 		step1Panel.setCSVFilePath(csvFilePath);
 	}
@@ -68,6 +93,7 @@ public class Step1Controller extends StepController {
 		if (fc.showOpenDialog(getStepPanel()) == JFileChooser.APPROVE_OPTION) {
 			step1Panel.setCSVFilePath(fc.getSelectedFile().getAbsolutePath());
 		}
+		// TODO update model and tell panel to update (here: enable next button)
 	}
 
 	private class CSVFileFilter extends FileFilter {
