a546658024040faa858c6402f92bf013ef6ff2df
EFI-52n <e.fiedler@52north.org>
2012-09-27 14:03:19 +0000

    test class

---
+++ 52n-sos-importer-feeder/src/test/java/org/n52/sos/importer/feeder/TestSensorObservationService.java
@@ -0,0 +1,14 @@
+package org.n52.sos.importer.feeder;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+
+public class TestSensorObservationService extends TestCase {
+
+	@Test
+	public void testSensorObservationService() {
+		
+	}
+
+}
