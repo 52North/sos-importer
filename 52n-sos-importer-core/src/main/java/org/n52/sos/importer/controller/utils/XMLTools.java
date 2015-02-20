45256e78e91adfb23ebce0536aba7ede33c1f544
EHJ-52n <e.h.juerrens@52north.org>
2015-02-20 17:00:12 +0100

    Update license header and license management

--- 52n-sos-importer-core/src/main/java/org/n52/sos/importer/controller/utils/XMLTools.java
+++ 52n-sos-importer-core/src/main/java/org/n52/sos/importer/controller/utils/XMLTools.java
@@ -1,39 +1,16 @@
 /**
- * Copyright (C) 2012
- * by 52North Initiative for Geospatial Open Source Software GmbH
- *
- * Contact: Andreas Wytzisk
- * 52 North Initiative for Geospatial Open Source Software GmbH
- * Martin-Luther-King-Weg 24
- * 48155 Muenster, Germany
- * info@52north.org
- *
- * This program is free software; you can redistribute and/or modify it under
- * the terms of the GNU General Public License version 2 as published by the
- * Free Software Foundation.
- *
- * This program is distributed WITHOUT ANY WARRANTY; even without the implied
- * WARRANTY OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program (see gnu-gpl v2.txt). If not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
- * visit the Free Software Foundation web page, http://www.fsf.org.
- */
-package org.n52.sos.importer.controller.utils;
-
-/**
- * {@link http://www.java2s.com/Code/Java/XML/CheckswhetherthesuppliedStringisanNCNameNamespaceClassifiedName.htm}
- * {@link http://www.openrdf.org/doc/sesame2/api/index.html?info/aduna/xml/XMLUtil.html}
- * @author <a href="mailto:e.h.juerrens@52north.org">Eike Hinderk J&uuml;rrens</a>
- */
-/*
  * Copyright Aduna (http://www.aduna-software.com/) (c) 1997-2006.
  *
  * Licensed under the Aduna BSD-style license.
  * 
  * Author not defined!
+ */
+package org.n52.sos.importer.controller.utils;
+
+/**
+ * {@link http://www.java2s.com/Code/Java/XML/CheckswhetherthesuppliedStringisanNCNameNamespaceClassifiedName.htm}
+ * 
+ * @author <a href="mailto:e.h.juerrens@52north.org">Eike Hinderk J&uuml;rrens</a>
  * 
  * TODO replace by org.n52.oxf.xml.XMLTools when OXF is used by this implementation
  */
