b1fa0321800e74c274ac2e0e8e6ba51a4f0a830d
EHJ-52n <e.h.juerrens@52north.org>
2018-10-24 16:15:47 +0200

    Minor code cleaning

--- wizard/src/main/java/org/n52/sos/importer/Start.java
+++ wizard/src/main/java/org/n52/sos/importer/Start.java
@@ -39,11 +39,6 @@ import org.n52.sos.importer.controller.Step1Controller;
  */
 public class Start {
 
-    /**
-     * <p>main.</p>
-     *
-     * @param args an array of {@link java.lang.String} objects.
-     */
     //CHECKSTYLE:OFF
     public static void main(String[] args) {
         //CHECKSTYLE:ON
